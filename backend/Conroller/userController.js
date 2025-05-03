0385aada5fb9c3306e6a82a990a04d23a14503fa
mmiskatul <masabimiskat@gmail.com>
2025-05-03 13:36:57 +0600

    getCurrent user function added

--- backend/Conroller/userController.js
+++ backend/Conroller/userController.js
@@ -1,68 +1,103 @@
 import mongoose from "mongoose";
-import User from '../Model/userModel.js'
-import validator from  'validator'
-import bcrypt, { hash } from 'bcrypt'
+import User from "../Model/userModel.js";
+import validator from "validator";
+import bcrypt, { hash } from "bcrypt";
 import { response } from "express";
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
+const JWT_SECRET = process.env.JWT_SECRET || "your_JWt_secret_here";
+const TOKEN_EXPIRES = "24h";
 
-const JWT_SECRET = process.env.JWT_SECRET ||  'your_JWt_secret_here';
-const  TOKEN_EXPIRES ='24h';
-
-const createToken =(userId)=>jwt.sign({id:userId},JWT_SECRET,{expiresIn:TOKEN_EXPIRES});
-
+const createToken = (userId) =>
+  jwt.sign({ id: userId }, JWT_SECRET, { expiresIn: TOKEN_EXPIRES });
 
 // REGISTER FUNCTION
-export async  function registerUser(req,res){
-    const {name,email,password} =req.body;
-    if(!name || !email || !password){
-        return res.status(400).json({success: false,massage :"All fields are required"});
+export async function registerUser(req, res) {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, massage: "All fields are required" });
+  }
+  if (!validator.isEmail(email)) {
+    return res.status(400).json({ success: false, massage: "Inavalid Email" });
+  }
+  if (password.length < 8) {
+    return res
+      .status(400)
+      .json({
+        success: false,
+        massage: "Password must Be atleast 8 characters ",
+      });
+  }
+  try {
+    if (await User.findOne({ email })) {
+      return res
+        .status(409)
+        .json({ success: false, massage: "User Already exits" });
     }
-    if(!validator.isEmail(email)){
-        return res.status(400).json({success: false ,massage:"Inavalid Email" });
-    }
-    if(password.length<8){
-        return res.status(400).json({success:false,massage:"Password must Be atleast 8 characters "});
-    }
-    try{
-        if(await User.findOne({email})){
-            return res.status(409).json({success:false,massage:"User Already exits"});
-        }
-        const  hashed=await bcrypt.hash(password,10);
-        const user =await User.create({name,email ,password :hashed});
-        const token =createToken(user._id);
+    const hashed = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashed });
+    const token = createToken(user._id);
 
-        res.status(201).json({success:true,token,user :{id:user._id, name:user.name ,email :user.email}});
-
-    }catch(err ){
-        console.log(err);
-        response.status(500).json({success :false,massage :"Server error"});
-    }
+    res
+      .status(201)
+      .json({
+        success: true,
+        token,
+        user: { id: user._id, name: user.name, email: user.email },
+      });
+  } catch (err) {
+    console.log(err);
+    response.status(500).json({ success: false, massage: "Server error" });
+  }
 }
 
-
-
 // LOGIN FUNCTION
-export async function loginUser(req,res) {
-    const {email,password} =req.body;
-    if( !email || !password){
-        return res.status(400).json({success: false,massage :"Email &  password required"});
+export async function loginUser(req, res) {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ success: false, massage: "Email &  password required" });
+  }
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, massage: "Invalid Credentials" });
     }
-    try{
-        const user=await User.findOne({email});
-        if(!user){
-            return res.status(401).json({success:false,massage:"Invalid Credentials"});
-        }
-        const  match =await bcrypt.compare(password,user.password);
-        if(!match){
-            return res.status(401).json({success:false,massage:"Invalid Credentials"});
-        }
-        const token =createToken(user._id);
-        res.json({success:true,token ,user:{id: user._id,name :user.name,email:user.email}})
-       
-        
-    }catch(err ){
-        console.log(err);
-        response.status(500).json({success :false,massage :"Server error"});
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res
+        .status(401)
+        .json({ success: false, massage: "Invalid Credentials" });
     }
+    const token = createToken(user._id);
+    res.json({
+      success: true,
+      token,
+      user: { id: user._id, name: user.name, email: user.email },
+    });
+  } catch (err) {
+    console.log(err);
+    response.status(500).json({ success: false, massage: "Server error" });
+  }
+}
+
+// GET CURRENT USER
+export async function getCurrentUser(req, res) {
+  try {
+    const user = await User.findById(req.user.id).select("name email ");
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, massage: "User Not found" });
+    }
+    res.json({ success: true, user });
+  } catch (err) {
+    console.log(err);
+    response.status(500).json({ success: false, massage: "Server error" });
+  }
 }
